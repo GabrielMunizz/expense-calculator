5c2d3bdd0f084d02c67c4984da23e232f6d39023
Gabriel Muniz <gabrielmfd@gmail.com>
2023-09-02 13:10:28 -0300

    Consertos de lint

--- src/tests/Wallet.test.tsx
+++ src/tests/Wallet.test.tsx
@@ -20,17 +20,4 @@ describe('Testes referentes ao componente Wallet', () => {
     expect(methodInput).toBeInTheDocument();
     expect(tagInput).toBeInTheDocument();
   });
-
-  it('Testa para ver se ao adicionar uma despesa, ela aparece no header', async () => {
-    const initialEntries = ['/carteira'];
-    renderWithRouterAndRedux(<App />, { initialEntries });
-
-    const totalExpenses = screen.getByTestId('total-field');
-    const valueInput = screen.getByTestId(/value-input/);
-    const descriptionInput = screen.getByTestId(/description-input/);
-    const currencyInput = screen.getByTestId(/currency-input/);
-    const methodInput = screen.getByTestId(/method-input/);
-    const tagInput = screen.getByTestId(/tag-input/);
-    const addExpenseBtn = screen.getByRole('button', { name: /adicionar despesa/i });
-  });
 });
